It loooks like I fixed the classification. I don't quite understand why it worked till now, cause the function that was to be maximized was minimized, and a wrong gradient was used

Code/gp_class_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from covariance_functions import covariance_mat
-from reg_parameters import data_params, common_params
+from class_parameters import data_params, common_params
 
 def plot_data(x, y, color1, color2):
     loc_y = y.reshape((y.shape[0],))
@@ -23,7 +23,7 @@ def sigmoid(x):
     return 1.0 / (1.0 + np.exp(- x))
 
 #Reassigning the parameters
-d, n = common_params.d, common_params.n
+d, n, t = common_params.d, common_params.n, common_params.t
 m = lambda x: np.zeros(x.shape[1])
 covariance_obj = data_params.cov_obj
 K = (covariance_obj).covariance_function
@@ -36,10 +36,10 @@ y_g = y_g.reshape((y_g.size, 1))
 y_g = sigmoid(y_g.reshape((y_g.size, 1)))
 y_g = np.sign(y_g - np.ones(y_g.shape) * 0.5)
 
-x_test = np.random.rand(d, n)
+x_test = np.random.rand(d, t)
 y_test = sample(m, K, x_test)
-y_test = sigmoid(y_g.reshape((y_g.size, 1)))
-y_test = np.sign(y_g - np.ones(y_g.shape) * 0.5)
+y_test = sigmoid(y_test.reshape((y_test.size, 1)))
+y_test = np.sign(y_test - np.ones(y_test.shape) * 0.5)
 
 
 if __name__ == "__main__":

Code/gp_classification.py
@@ -7,20 +7,22 @@ import scipy.optimize as opt
 
 
 def logistic_loss(f, y):
-    """Logistic loss function"""
+    """log p(y|f)"""
     f = f.reshape((f.size, 1))
-    return (- np.sum(np.log(np.exp(-y * f) + np.ones(y.shape))))
+    return np.sum(np.log(np.exp(-y * f) + np.ones(y.shape)))
 
 
 def logistic_likelyhood_hessian(f):
-    """Hessian of the p(y|f)"""
+    """Hessian of the log p(y|f)"""
     f = f.reshape((f.size, 1))
     diag_vec = (-np.exp(f) / np.square(np.ones(f.shape) + np.exp(f)))
-    return np.diag(diag_vec.reshape((diag_vec.size, )))
+    return -np.diag(diag_vec.reshape((diag_vec.size, )))
+
 
 def logistic_grad(f, y):
     f = f.reshape((f.size, 1))
-    return (y / ( np.exp(-y * f)))
+    return -((y + np.ones(y.shape))/2 - sigmoid(f))
+    # return -y / (np.exp(-y * f))
 
 
 #Reassigning the parameters
@@ -38,13 +40,17 @@ K_test = covariance_mat(K, x_test, x_test)
 
 #Optimization
 f_0 = np.zeros(y_g.shape)
+print(logistic_loss(f_0, y_g))
 res = opt.minimize(fun=(lambda f: logistic_loss(f, y_g)), x0=f_0.reshape((f_0.size,)), method='Newton-CG',
-                   jac=lambda f: logistic_grad(f, y_g).reshape((f_0.size,)), hess=logistic_likelyhood_hessian, options={'maxiter': 10})
+                   jac=lambda f: logistic_grad(f, y_g).reshape((f_0.size,)), hess=logistic_likelyhood_hessian, options={'disp':True})
 f_opt = res['x']
+print(logistic_loss(f_opt, y_g))
+# f_opt = f_0
 
 #Calculating the classification results on the grid
 f_test = np.dot(np.dot(K_test_x, np.linalg.inv(K_x)), f_opt)
-y_test = sigmoid(f_test.reshape((f_test.size, 1)))
-app_y_test = np.sign(y_test - np.ones(y_test.shape) * 0.5)
-
+app_y_test = sigmoid(f_test.reshape((f_test.size, 1)))
+app_y_test = np.sign(app_y_test - np.ones(y_test.shape) * 0.5)
 print(np.linalg.norm(app_y_test - y_test))
+# print(app_y_test - y_test)
+