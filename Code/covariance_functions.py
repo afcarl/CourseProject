I fixed the evaluation of the derivatives for ml of gp-regression. I've also implemented the evaluation of ml and it's derivatives for ml-classification

Code/covariance_functions.py
@@ -3,13 +3,18 @@ import math
 from scipy.optimize import check_grad
 from scipy.special import gamma, kv
 from abc import ABCMeta, abstractmethod
+import scipy as sp
 
 # General functions
 
 
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(- x))
+
+
 def delta(x, y):
     """Delta-function"""
-    return np.sum(x == y, axis=0)
+    return np.sign(np.sum(x == y, axis=0))
 
 
 def gaussian_noise_term(noise_variance, x, y):
@@ -24,10 +29,11 @@ def add_noise(cov_func, noise_variance):
 
 def covariance_mat(covariance_func, x, y):
     """Computing covariance matrix for given covariance function and point arrays"""
-    mat = np.zeros((x.shape[1], y.shape[1]))
-    for i in range(0, x.shape[1]):
-        for j in range(0, y.shape[1]):
-            mat[i, j] = covariance_func(x[:, i], y[:, j])
+    # mat = np.zeros((x.shape[1], y.shape[1]))
+    # for i in range(0, x.shape[1]):
+    #     for j in range(0, y.shape[1]):
+    #         mat[i, j] = covariance_func(x[:, i], y[:, j])
+    # print(mat-covariance_func(x[:, :, None], y[:, None, :]))
     return covariance_func(x[:, :, None], y[:, None, :])
 
 # Specific covariance functions
@@ -49,13 +55,26 @@ class CovarianceFamily:
         pass
 
     @abstractmethod
+    def class_ml_oracle(self, x, y, f_opt, hess_opt, w=np.NaN):
+        """
+        Oracle function for marginal likelihood for gp-classification
+        :param x: data set
+        :param y: labels vector
+        :param f_opt: posterior (p(f|X, y)) mode
+        :param hess_opt: -\nabla\nabla log p(y|f)|_{f_opt}
+        :param w: hyper-parameters vector
+        :return: a tuple (the marginal likelihood, marginal likelihood gradient with respect to hyper-parameters)
+        """
+        pass
+
+    @abstractmethod
     def oracle(self, x, y, w=np.NaN):
         """
         Oracle function for marginal likelihood for gp-regression
         :param x: an array of data-points
         :param y: a vector of target values
         :param w: a vector of covariance functions' family hyper-parameters
-        :return: a tuple (the marginal likelihood, marginal likelihood gradient with respect to hyperparameters)
+        :return: a tuple (the marginal likelihood, marginal likelihood gradient with respect to hyper-parameters)
         """
         pass
 
@@ -76,11 +95,23 @@ class CovarianceFamily:
         pass
 
     @staticmethod
+    def _get_class_ml(y, f_opt, b, k_inv):
+        """
+        :param y: label vector
+        :param f_opt: posterior mode
+        :param b: I + W^(1/2) K W^(1/2), where W is minus hessian of the log likelihood at f_opt
+        :param k_inv: inverse covariance matrix at data points
+        :return: the Laplace estimation of log marginal likelihood
+        """
+        return -((f_opt.T.dot(k_inv)).dot(f_opt) + np.log(np.linalg.det(b)))/2
+    # - np.sum(np.log(np.exp(-y * f_opt) + np.ones(y.shape)))
+
+    @staticmethod
     def _get_ml(y, k_inv):
         """
         :param y: target value vector
         :param k_inv: inverse covariance matrix at data points
-        :return: marginal likelihood
+        :return: log marginal likelihood
         """
         n = y.size
         return -((y.T.dot(k_inv)).dot(y) - np.log(np.linalg.det(k_inv)) + n * np.log(2 * math.pi))/2
@@ -96,12 +127,37 @@ class CovarianceFamily:
         """
         return ((((y.T.dot(k_inv)).dot(dk_dtheta_mat)).dot(k_inv)).dot(y) - np.trace(k_inv.dot(dk_dtheta_mat))) / 2
 
+    @staticmethod
+    def _get_class_ml_partial_derivative(f_opt, k_inv, ancillary_mat, dk_dtheta_mat):
+        """
+        :param y: target value vector
+        :param f_opt: posterior mode
+        :param k_inv: inverse covariance matrix at data points
+        :param ancillary_mat: (W^(-1) + K)^(-1), where W is hess_opt
+        :param dk_dtheta_mat: the matrix of partial derivatives of the covariance function at data points
+        with respect to hyper-parameter theta
+        :return: marginal likelihood partial derivative with respect to hyper-parameter theta
+        """
+        return ((((f_opt.T.dot(k_inv)).dot(dk_dtheta_mat)).dot(k_inv)).dot(f_opt) -
+                np.trace(ancillary_mat.dot(dk_dtheta_mat)))/2
+
     def _get_k_inv(self, x):
         """
         :param x: data points array
         :return: inverse covariance matrix at data points
         """
         return np.linalg.inv(covariance_mat(self.covariance_function, x, x))
+
+    @staticmethod
+    def _get_b(k, hess_opt):
+        """
+        :param k: covariance matrix
+        :param hess_opt: -\nabla\nabla log p(y|f)|_{f_opt}
+        :return: I + W^(1/2) K W^(1/2), where W is minus hessian of the log likelihood at f_opt
+        """
+        w = sp.linalg.sqrtm(hess_opt)
+        b = (w.dot(k)).dot(w)
+        return np.eye(b.shape[0]) + b
 
 
 class SquaredExponential(CovarianceFamily):
@@ -128,7 +184,34 @@ class SquaredExponential(CovarianceFamily):
             l = w[1]
             sigma_l = w[2]
         r = np.linalg.norm(x - y, axis=0)
-        return np.exp(-np.square(r) / (2*(l**2))) * sigma_f**2 + gaussian_noise_term(sigma_l, x, y)
+        return np.exp(-r**2 / (2*(l**2))) * sigma_f**2 + gaussian_noise_term(sigma_l, x, y)
+
+    def class_ml_oracle(self, x, y, f_opt, hess_opt, w=np.NaN):
+        if np.all(np.isnan(w)):
+            l = self.l
+            sigma_f = self.sigma_f
+            sigma_l = self.sigma_l
+        else:
+            sigma_f = w[0]
+            l = w[1]
+            sigma_l = w[2]
+
+        n = y.size
+        se = SquaredExponential(sigma_f, l, sigma_l)
+        k_inv = se._get_k_inv(x)
+        k = covariance_mat(se.covariance_function, x, x)
+        anc_mat = np.linalg.inv(np.linalg.inv(hess_opt) + k)
+        b = se._get_b(k, hess_opt)
+        ml = se._get_class_ml(y, f_opt, b, k_inv)
+
+        dk_dl_mat = covariance_mat(se._dse_dl, x, x)
+        dk_dsigmaf_mat = covariance_mat(se._dse_dsigmaf, x, x)
+        dk_dsigmal_mat = 2 * sigma_l * np.eye(n)
+
+        g_1 = se._get_class_ml_partial_derivative(f_opt, k_inv, anc_mat, dk_dsigmaf_mat)
+        g_2 = se._get_class_ml_partial_derivative(f_opt, k_inv, anc_mat, dk_dl_mat)
+        g_3 = se._get_class_ml_partial_derivative(f_opt, k_inv, anc_mat, dk_dsigmal_mat)
+        return -ml, -np.array([g_1, g_2, g_3]).reshape(3,)
 
     def oracle(self, X, y, w=np.NaN):
         if np.all(np.isnan(w)):
@@ -145,21 +228,22 @@ class SquaredExponential(CovarianceFamily):
         k_inv = se._get_k_inv(x=X)
         ml = se._get_ml(y, k_inv)
 
-        def d_se_dl(x, y):
-            r = np.linalg.norm(x - y, axis=0)
-            return (np.exp(-np.square(r) / (2*(l**2))) * sigma_f**2) * (r / (l ** 3))
-
-        def d_se_dsigmaf(x, y):
-            r = np.linalg.norm(x - y, axis=0)
-            return 2 * sigma_f * np.exp(-np.square(r) / (2*(l**2)))
-
-        dk_dl_mat = covariance_mat(d_se_dl, X, X)
-        dk_dsigmaf_mat = covariance_mat(d_se_dsigmaf, X, X)
+        dk_dl_mat = covariance_mat(se._dse_dl, X, X)
+        dk_dsigmaf_mat = covariance_mat(se._dse_dsigmaf, X, X)
         dk_dsigmal_mat = 2 * sigma_l * np.eye(n)
+
         g_1 = se._get_ml_partial_derivative(y, k_inv, dk_dsigmaf_mat)
         g_2 = se._get_ml_partial_derivative(y, k_inv, dk_dl_mat)
         g_3 = se._get_ml_partial_derivative(y, k_inv, dk_dsigmal_mat)
         return -ml, -np.array([g_1, g_2, g_3]).reshape(3,)
+
+    def _dse_dl(self, x, y):
+        r = np.linalg.norm(x - y, axis=0)
+        return (np.exp(-r**2 / (2*(self.l**2))) * self.sigma_f**2) * (r**2 / (self.l ** 3))
+
+    def _dse_dsigmaf(self, x, y):
+        r = np.linalg.norm(x - y, axis=0)
+        return 2 * self.sigma_f * np.exp(-r**2 / (2*(self.l**2)))
 
 
 class GammaExponential(CovarianceFamily):
@@ -220,7 +304,7 @@ class GammaExponential(CovarianceFamily):
             r = np.linalg.norm(x - y, axis=0)
             if r == 0:
                 return 0
-            return - sigma_f**2 * (r/l)**gamma * np.exp(-(r/l)**gamma) * np.log(r/l)
+            return -sigma_f**2 * (r/l)**gamma * np.exp(-(r/l)**gamma) * np.log(r/l)
 
         dk_dl_mat = covariance_mat(d_ge_dl, X, X)
         dk_dsigmaf_mat = covariance_mat(d_ge_dsigmaf, X, X)
@@ -260,13 +344,13 @@ def rational_quadratic_cov(alpha, l):
 
 if __name__ == '__main__':
     sigma_f = 1.2
-    sigma_l = 0.5
+    sigma_l = 0.1
     l = 0.2
     gamma = 0.8
 
     w0 = np.array([sigma_f, l, sigma_l])
     se = SquaredExponential(sigma_f, l, sigma_l)
-    x = np.array([[1., 2., 3.]])
+    x = np.array([[1., 2., 3.], [4., 5., 6.]])
     y = np.array([0., 1., 0.])
     # print (covariance_mat(ge.covariance_function, x, x))
     def func(w):

Code/gp_class_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from covariance_functions import covariance_mat
+from covariance_functions import covariance_mat, sigmoid
 from class_parameters import data_params, common_params
 
 def plot_data(x, y, color1, color2):
@@ -18,9 +18,6 @@ def sample(mean_func, cov_func, x):
     y = np.random.multivariate_normal(mean_vector, cov_mat)
     return y
 
-
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(- x))
 
 #Reassigning the parameters
 d, n, t = common_params.d, common_params.n, common_params.t

Code/gp_classification.py
@@ -4,6 +4,7 @@ from covariance_functions import covariance_mat
 from gp_class_data import x_g, y_g, plot_data, x_test, y_test, sigmoid
 from class_parameters import model_params, common_params
 import scipy.optimize as opt
+from scipy.optimize import check_grad
 
 
 def logistic_loss(f, y):
@@ -40,17 +41,29 @@ K_test = covariance_mat(K, x_test, x_test)
 
 #Optimization
 f_0 = np.zeros(y_g.shape)
-print(logistic_loss(f_0, y_g))
 res = opt.minimize(fun=(lambda f: logistic_loss(f, y_g)), x0=f_0.reshape((f_0.size,)), method='Newton-CG',
-                   jac=lambda f: logistic_grad(f, y_g).reshape((f_0.size,)), hess=logistic_likelyhood_hessian, options={'disp':True})
+                   jac=lambda f: logistic_grad(f, y_g).reshape((f_0.size,)), hess=logistic_likelyhood_hessian, options={'disp':False})
 f_opt = res['x']
-print(logistic_loss(f_opt, y_g))
-# f_opt = f_0
+
+
+def func(w):
+    loss, gradient = covariance_obj.oracle(x_g, y_g, w)
+    return loss
+
+def grad(w):
+    loss, gradient = covariance_obj.oracle(x_g, y_g, w)
+    return gradient
+
+# w0 = covariance_obj.get_params() + np.array([0.5, 0.5, 0.5])
+# loss_1 = func(w0)
+# grad_1 = grad(w0)
+# loss_eps = func(w0 + np.array([1e-6, 0, 0]))
+# print((loss_eps - loss_1)*1e6, grad_1)
+# print(check_grad(func, grad, w0))
 
 #Calculating the classification results on the grid
 f_test = np.dot(np.dot(K_test_x, np.linalg.inv(K_x)), f_opt)
 app_y_test = sigmoid(f_test.reshape((f_test.size, 1)))
 app_y_test = np.sign(app_y_test - np.ones(y_test.shape) * 0.5)
-print(np.linalg.norm(app_y_test - y_test))
-# print(app_y_test - y_test)
+# print(np.linalg.norm(app_y_test - y_test))
 

Code/gp_regression.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import scipy.optimize as op
 from sklearn import svm
 import time
+import scipy as sp
 
 from covariance_functions import delta, covariance_mat
-# from gp_reg_data import x_g, y_g, plot_data, x_test, y_test
+from gp_reg_data import x_g, y_g, plot_data, x_test, y_test
 from reg_parameters import model_params, common_params
 # from optimization import full_gradient_descent, Problem
-from real_reg_data import x_g, y_g, x_test, y_test
+# from real_reg_data import x_g, y_g, x_test, y_test
 
 
 def sample_for_matrices(mean_vec, cov_mat):
@@ -58,6 +59,14 @@ def grad(w):
 # print(optimal_params)
 
 bnds = ((1e-2, None), (1e-2, None), (1e-2, None))
+
+# w0 = covariance_obj.get_params()
+# loss_1 = fun(w0)
+# grad_1 = grad(w0)
+# loss_eps = fun(w0 + np.array([0, 0, 1e-8]))
+# print((loss_eps - loss_1)*1e8, grad_1)
+# print(sp.optimize.check_grad(fun, grad, w0))
+
 start = time.clock()
 res = op.minimize(fun, covariance_obj.get_params(), args=(), method='L-BFGS-B', jac=grad, bounds=bnds,
             options={'ftol':1e-3, 'disp':False})