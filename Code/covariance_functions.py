67662ecda3e863226a2c59789597e8f8043bff50
Pavel Izmailov <izmailovpavel@gmail.com>
2015-10-11 00:57:23 +0300

    Added modules for regression and classification, also fixed the optimization srashing issue

--- Code/covariance_functions.py
+++ Code/covariance_functions.py
@@ -29,11 +29,6 @@ def add_noise(cov_func, noise_variance):
 
 def covariance_mat(covariance_func, x, y):
     """Computing covariance matrix for given covariance function and point arrays"""
-    # mat = np.zeros((x.shape[1], y.shape[1]))
-    # for i in range(0, x.shape[1]):
-    #     for j in range(0, y.shape[1]):
-    #         mat[i, j] = covariance_func(x[:, i], y[:, j])
-    # print(mat-covariance_func(x[:, :, None], y[:, None, :]))
     return covariance_func(x[:, :, None], y[:, None, :])
 
 # Specific covariance functions
@@ -55,30 +50,6 @@ class CovarianceFamily:
         pass
 
     @abstractmethod
-    def class_ml_oracle(self, x, y, f_opt, hess_opt, w=np.NaN):
-        """
-        Oracle function for marginal likelihood for gp-classification
-        :param x: data set
-        :param y: labels vector
-        :param f_opt: posterior (p(f|X, y)) mode
-        :param hess_opt: -\nabla\nabla log p(y|f)|_{f_opt}
-        :param w: hyper-parameters vector
-        :return: a tuple (the marginal likelihood, marginal likelihood gradient with respect to hyper-parameters)
-        """
-        pass
-
-    @abstractmethod
-    def oracle(self, x, y, w=np.NaN):
-        """
-        Oracle function for marginal likelihood for gp-regression
-        :param x: an array of data-points
-        :param y: a vector of target values
-        :param w: a vector of covariance functions' family hyper-parameters
-        :return: a tuple (the marginal likelihood, marginal likelihood gradient with respect to hyper-parameters)
-        """
-        pass
-
-    @abstractmethod
     def get_params(self):
         """
         :return: The hyper-parameters vector of the CovarianceFamily object
@@ -94,82 +65,33 @@ class CovarianceFamily:
         """
         pass
 
-    @staticmethod
-    def _get_class_ml(y, f_opt, b, k_inv):
+    @abstractmethod
+    def get_derivative_function_list(self, params):
         """
-        :param y: label vector
-        :param f_opt: posterior mode
-        :param b: I + W^(1/2) K W^(1/2), where W is minus hessian of the log likelihood at f_opt
-        :param k_inv: inverse covariance matrix at data points
-        :return: the Laplace estimation of log marginal likelihood
+        :return: a list of functions, which produce the derivatives of the covariance matrix with respect to
+        hyper-parameters, when given to the covariance_matrix() function
         """
-        return -((f_opt.T.dot(k_inv)).dot(f_opt) + np.log(np.linalg.det(b)))/2
-    # - np.sum(np.log(np.exp(-y * f_opt) + np.ones(y.shape)))
-
-    @staticmethod
-    def _get_ml(y, k_inv):
-        """
-        :param y: target value vector
-        :param k_inv: inverse covariance matrix at data points
-        :return: log marginal likelihood
-        """
-        n = y.size
-        return -((y.T.dot(k_inv)).dot(y) - np.log(np.linalg.det(k_inv)) + n * np.log(2 * math.pi))/2
-
-    @staticmethod
-    def _get_ml_partial_derivative(y, k_inv, dk_dtheta_mat):
-        """
-        :param y: target value vector
-        :param k_inv: inverse covariance matrix at data points
-        :param dk_dtheta_mat: the matrix of partial derivatives of the covariance function at data points
-        with respect to hyper-parameter theta
-        :return: marginal likelihood partial derivative with respect to hyper-parameter theta
-        """
-        return ((((y.T.dot(k_inv)).dot(dk_dtheta_mat)).dot(k_inv)).dot(y) - np.trace(k_inv.dot(dk_dtheta_mat))) / 2
-
-    @staticmethod
-    def _get_class_ml_partial_derivative(f_opt, k_inv, ancillary_mat, dk_dtheta_mat):
-        """
-        :param y: target value vector
-        :param f_opt: posterior mode
-        :param k_inv: inverse covariance matrix at data points
-        :param ancillary_mat: (W^(-1) + K)^(-1), where W is hess_opt
-        :param dk_dtheta_mat: the matrix of partial derivatives of the covariance function at data points
-        with respect to hyper-parameter theta
-        :return: marginal likelihood partial derivative with respect to hyper-parameter theta
-        """
-        return ((((f_opt.T.dot(k_inv)).dot(dk_dtheta_mat)).dot(k_inv)).dot(f_opt) -
-                np.trace(ancillary_mat.dot(dk_dtheta_mat)))/2
-
-    def _get_k_inv(self, x):
-        """
-        :param x: data points array
-        :return: inverse covariance matrix at data points
-        """
-        return np.linalg.inv(covariance_mat(self.covariance_function, x, x))
-
-    @staticmethod
-    def _get_b(k, hess_opt):
-        """
-        :param k: covariance matrix
-        :param hess_opt: -\nabla\nabla log p(y|f)|_{f_opt}
-        :return: I + W^(1/2) K W^(1/2), where W is minus hessian of the log likelihood at f_opt
-        """
-        w = sp.linalg.sqrtm(hess_opt)
-        b = (w.dot(k)).dot(w)
-        return np.eye(b.shape[0]) + b
+        pass
 
 
 class SquaredExponential(CovarianceFamily):
     """A class, representing the squared-exponential covariance functions family."""
 
-    def __init__(self, sigma_f, l, noise):
-        self.sigma_f, self.l, self.sigma_l = sigma_f, l, noise
+    def __init__(self, params):
+        if params.size > 3:
+            raise ValueError("Wrong parameters for SquaredExponential")
+
+        self.sigma_f = params[0]
+        self.l = params[1]
+        self.sigma_l = params[2]
 
     def get_params(self):
         return np.array([self.sigma_f, self.l, self.sigma_l])
 
     def set_params(self, params):
+        if params.size > 3:
+            raise ValueError("Wrong parameters for SquaredExponential")
+
         self.sigma_f = params[0]
         self.l = params[1]
         self.sigma_l = params[2]
@@ -186,57 +108,6 @@ class SquaredExponential(CovarianceFamily):
         r = np.linalg.norm(x - y, axis=0)
         return np.exp(-r**2 / (2*(l**2))) * sigma_f**2 + gaussian_noise_term(sigma_l, x, y)
 
-    def class_ml_oracle(self, x, y, f_opt, hess_opt, w=np.NaN):
-        if np.all(np.isnan(w)):
-            l = self.l
-            sigma_f = self.sigma_f
-            sigma_l = self.sigma_l
-        else:
-            sigma_f = w[0]
-            l = w[1]
-            sigma_l = w[2]
-
-        n = y.size
-        se = SquaredExponential(sigma_f, l, sigma_l)
-        k_inv = se._get_k_inv(x)
-        k = covariance_mat(se.covariance_function, x, x)
-        anc_mat = np.linalg.inv(np.linalg.inv(hess_opt) + k)
-        b = se._get_b(k, hess_opt)
-        ml = se._get_class_ml(y, f_opt, b, k_inv)
-
-        dk_dl_mat = covariance_mat(se._dse_dl, x, x)
-        dk_dsigmaf_mat = covariance_mat(se._dse_dsigmaf, x, x)
-        dk_dsigmal_mat = 2 * sigma_l * np.eye(n)
-
-        g_1 = se._get_class_ml_partial_derivative(f_opt, k_inv, anc_mat, dk_dsigmaf_mat)
-        g_2 = se._get_class_ml_partial_derivative(f_opt, k_inv, anc_mat, dk_dl_mat)
-        g_3 = se._get_class_ml_partial_derivative(f_opt, k_inv, anc_mat, dk_dsigmal_mat)
-        return -ml, -np.array([g_1, g_2, g_3]).reshape(3,)
-
-    def oracle(self, X, y, w=np.NaN):
-        if np.all(np.isnan(w)):
-            l = self.l
-            sigma_f = self.sigma_f
-            sigma_l = self.sigma_l
-        else:
-            sigma_f = w[0]
-            l = w[1]
-            sigma_l = w[2]
-
-        n = y.size
-        se = SquaredExponential(sigma_f, l, sigma_l)
-        k_inv = se._get_k_inv(x=X)
-        ml = se._get_ml(y, k_inv)
-
-        dk_dl_mat = covariance_mat(se._dse_dl, X, X)
-        dk_dsigmaf_mat = covariance_mat(se._dse_dsigmaf, X, X)
-        dk_dsigmal_mat = 2 * sigma_l * np.eye(n)
-
-        g_1 = se._get_ml_partial_derivative(y, k_inv, dk_dsigmaf_mat)
-        g_2 = se._get_ml_partial_derivative(y, k_inv, dk_dl_mat)
-        g_3 = se._get_ml_partial_derivative(y, k_inv, dk_dsigmal_mat)
-        return -ml, -np.array([g_1, g_2, g_3]).reshape(3,)
-
     def _dse_dl(self, x, y):
         r = np.linalg.norm(x - y, axis=0)
         return (np.exp(-r**2 / (2*(self.l**2))) * self.sigma_f**2) * (r**2 / (self.l ** 3))
@@ -244,6 +115,10 @@ class SquaredExponential(CovarianceFamily):
     def _dse_dsigmaf(self, x, y):
         r = np.linalg.norm(x - y, axis=0)
         return 2 * self.sigma_f * np.exp(-r**2 / (2*(self.l**2)))
+
+    def get_derivative_function_list(self, params):
+        se = SquaredExponential(params)
+        return [se._dse_dsigmaf, se._dse_dl]
 
 
 class GammaExponential(CovarianceFamily):
@@ -275,46 +150,6 @@ class GammaExponential(CovarianceFamily):
         r = np.linalg.norm(x - y, axis=0)
         return np.exp(-(r / l)**gamma) * sigma_f**2 + gaussian_noise_term(sigma_l, x, y)
 
-    def oracle(self, X, y, w=np.NaN):
-        if np.all(np.isnan(w)):
-            l = self.l
-            sigma_f = self.sigma_f
-            gamma = self.gamma
-            sigma_l = self.sigma_l
-        else:
-            sigma_f = w[0]
-            l = w[1]
-            gamma = w[2]
-            sigma_l = w[3]
-
-        n = y.size
-        ge = GammaExponential(sigma_f, l, gamma, sigma_l)
-        k_inv = ge._get_k_inv(x=X)
-        ml = ge._get_ml(y, k_inv)
-
-        def d_ge_dl(x, y):
-            r = np.linalg.norm(x - y, axis=0)
-            return sigma_f**2 * (r / l)**gamma * np.exp(-(r/l)**gamma) / l
-
-        def d_ge_dsigmaf(x, y):
-            r = np.linalg.norm(x - y, axis=0)
-            return sigma_f * 2 * np.exp(-(r / l)**gamma)
-
-        def d_ge_dgamma(x, y):
-            r = np.linalg.norm(x - y, axis=0)
-            if r == 0:
-                return 0
-            return -sigma_f**2 * (r/l)**gamma * np.exp(-(r/l)**gamma) * np.log(r/l)
-
-        dk_dl_mat = covariance_mat(d_ge_dl, X, X)
-        dk_dsigmaf_mat = covariance_mat(d_ge_dsigmaf, X, X)
-        dk_dgamma_mat = covariance_mat(d_ge_dgamma, X, X)
-        dk_dsigmal_mat = 2 * sigma_l * np.eye(n)
-        g_1 = ge._get_ml_partial_derivative(y, k_inv, dk_dl_mat)
-        g_2 = ge._get_ml_partial_derivative(y, k_inv, dk_dsigmaf_mat)
-        g_3 = ge._get_ml_partial_derivative(y, k_inv, dk_dgamma_mat)
-        g_4 = ge._get_ml_partial_derivative(y, k_inv, dk_dsigmal_mat)
-        return -ml, -np.array([g_2, g_1, g_3, g_4]).reshape(4,)
 
 def matern_cov(nu, l):
     """Matern covariance function"""
--- Code/gaussian_process.py
+++ Code/gaussian_process.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import math
 import scipy.optimize as op
 import copy
+import scipy as sp
 
-from covariance_functions import CovarianceFamily, covariance_mat, sigmoid, SquaredExponential
+from covariance_functions import CovarianceFamily, covariance_mat, sigmoid
 
 
 def gp_plot_reg_data(x, y, color):
@@ -60,7 +61,6 @@ class GaussianProcess:
         """
         :param dim: dimensions of the generated data
         :param num: number of generated data pointts
-        :param class_reg: regression or classification
         :return: tuple (data_points, labels or target values)
         """
         if dim <= 0:
@@ -71,13 +71,10 @@ class GaussianProcess:
         if not (seed is None):
             np.random.seed(seed)
         points = np.random.rand(dim, num)
-        if dim == 1:
-            points = np.sort(points)
         targets = self.sample(self.mean_fun, self.covariance_fun, points, seed)
         if self.type == "class":
-            targets = sigmoid(targets)
-            targets = np.sign(targets - np.ones(targets.shape) * 0.5)
-        return points, targets.reshape(targets.size, 1)
+            targets = np.sign(targets)
+        return points, targets.reshape((targets.size, 1))
 
     @staticmethod
     def sample(mean_func, cov_func, points, seed=None):
@@ -112,7 +109,7 @@ class GaussianProcess:
         if not (isinstance(mean_vec, np.ndarray) and
                 isinstance(cov_mat, np.ndarray)):
             raise TypeError("points must be a numpy array")
-        y = np.random.multivariate_normal(mean_vec.reshape((mean_vec.size,)), cov_mat)
+        # y = np.random.multivariate_normal(mean_vec.reshape((mean_vec.size,)), cov_mat)
         upper_bound = mean_vec + 3 * np.sqrt(np.diagonal(cov_mat).reshape(mean_vec.shape))
         lower_bound = mean_vec - 3 * np.sqrt(np.diagonal(cov_mat).reshape(mean_vec.shape))
         return mean_vec, upper_bound, lower_bound
@@ -121,7 +118,7 @@ class GaussianProcess:
     def _reg_get_ml(targets, cov_inv, cov_l):
         """
         :param targets: target_values list
-        :param points_inv: inversed covariance at data points
+        :param cov_inv: inversed covariance at data points
         :return: marginal likelihood
         """
         n = targets.size
@@ -131,8 +128,8 @@ class GaussianProcess:
     @staticmethod
     def _reg_get_ml_partial_derivative(targets, cov_inv, dk_dtheta_mat):
         """
-        :param y: target value vector
-        :param k_inv: inverse covariance matrix at data points
+        :param targets: target value vector
+        :param cov_inv: inverse covariance matrix at data points
         :param dk_dtheta_mat: the matrix of partial derivatives of the covariance function at data points
         with respect to hyper-parameter theta
         :return: marginal likelihood partial derivative with respect to hyper-parameter theta
@@ -166,24 +163,12 @@ class GaussianProcess:
         points_l = np.linalg.cholesky(points_cov)
         points_l_inv = np.linalg.inv(points_l)
         points_cov_inv = points_l_inv.T.dot(points_l_inv)
-        marginal_likelihood = self._reg_get_ml(targets, points_cov_inv, points_l)[0, 0]
+        marginal_likelihood = self._reg_get_ml(targets, points_cov_inv, points_l)
         gradient = self._reg_get_ml_grad(points, targets, points_cov_inv, params)
         gradient = gradient.reshape(gradient.size, )
         return marginal_likelihood, gradient
 
-    def oracle(self, points, targets, params=None):
-        """
-        :param points: data points array
-        :param targets: target values vector
-        :param params: hyper-parameters vector
-        """
-        if params is None:
-            params = self.covariance_obj.get_params()
-        if self.type == "reg":
-            return self._reg_oracle(points, targets, params)
-        return 0
-
-    def find_hyper_parameters(self, data_points, target_values):
+    def _reg_find_hyper_parameters(self, data_points, target_values):
         """
         Optimizes covariance hyper-parameters
         :param data_points: an array of data points
@@ -195,11 +180,11 @@ class GaussianProcess:
             raise TypeError("The operands must be of type numpy array")
 
         def loc_fun(w):
-            loss, grad = self.oracle(data_points, target_values, w)
+            loss, grad = self._reg_oracle(data_points, target_values, w)
             return -loss
 
         def loc_grad(w):
-            loss, grad = self.oracle(data_points, target_values, w)
+            loss, grad = self._reg_oracle(data_points, target_values, w)
             return -grad
 
         # print(self.covariance_obj.get_params())
@@ -207,12 +192,12 @@ class GaussianProcess:
         # print(loc_fun(self.covariance_obj.get_params()))
         # exit()
         bnds = ((1e-2, None), (1e-2, None), (1e-5, None))
-        res = op.minimize(loc_fun, self.covariance_obj.get_params(), args=(), method='L-BFGS-B', jac=loc_grad, bounds=bnds,
-            options={'gtol': 1e-5, 'disp': False})
+        res = op.minimize(loc_fun, self.covariance_obj.get_params(), args=(), method='L-BFGS-B', jac=loc_grad,
+                          bounds=bnds, options={'gtol': 1e-5, 'disp': True})
         optimal_params = res.x
         self.covariance_obj.set_params(optimal_params)
 
-    def predict(self, test_points, training_points, training_targets):
+    def _reg_predict(self, test_points, training_points, training_targets):
         """
         :param test_points: array of test points
         :param training_points: training set array
@@ -235,21 +220,154 @@ class GaussianProcess:
         gp_plot_reg_data(test_points, test_targets, 'b')
         return test_targets
 
-if __name__ == "__main__":
-    data_params = np.array([1.0, 0.2, 0.1])
-    data_covariance_obj = SquaredExponential(data_params)
-    gp = GaussianProcess(data_covariance_obj, lambda x: 0, 'reg')
-    num = 1000
-    test_num = 2000
-    dim = 1
-    x_tr, y_tr = gp.generate_data(dim, num, seed=21)
-    x_test, y_test = gp.generate_data(dim, test_num, seed = 10)
-    model_params = np.array([2.0, 0.8, 0.4])
-    model_covariance_obj = SquaredExponential(model_params)
-    new_gp = GaussianProcess(model_covariance_obj, lambda x: 0, 'reg')
-    new_gp.find_hyper_parameters(x_tr, y_tr)
-    print(new_gp.covariance_obj.get_params())
-    predicted_y_test = new_gp.predict(x_test, x_tr, y_tr)
-    print(np.linalg.norm(y_test - predicted_y_test))
-    gp_plot_reg_data(x_tr, y_tr, 'bx')
-    plt.show()
+    @staticmethod
+    def _class_get_ml(f_opt, b, cov_inv):
+        """
+        :param f_opt: posterior mode
+        :param b: I + W^(1/2) K W^(1/2), where W is minus hessian of the log likelihood at f_opt
+        :param cov_inv: inverse covariance matrix at data points
+        :return: the Laplace estimation of log marginal likelihood
+        """
+        l_b = np.linalg.cholesky(b)
+        return -((f_opt.T.dot(cov_inv)).dot(f_opt) + 2 * np.sum(np.log(np.diag(l_b))))/2
+
+    @staticmethod
+    def _get_b(points_cov, hess_opt):
+        """
+        :param points_cov: covariance matrix
+        :param hess_opt: -\nabla\nabla log p(y|f)|_{f_opt}
+        :return: I + W^(1/2) K W^(1/2), where W is minus hessian of the log likelihood at f_opt
+        """
+        w = sp.linalg.sqrtm(hess_opt)
+        b = (w.dot(points_cov)).dot(w)
+        return np.eye(b.shape[0]) + b
+
+    @staticmethod
+    def _get_laplace_approximation(labels, cov_inv, cov_l):
+        """
+        :param labels: label vector
+        :param cov_inv: inverse covariance matrix at data points
+        :return: tuple (posterior mode f_opt, unnormalized posterior hessian at f_opt)
+        """
+        def loss(f):
+            """log p(y|f)"""
+            f = f.reshape((f.size, 1))
+            return (np.sum(np.log(np.exp(-labels * f) + np.ones(labels.shape))) + (f.T.dot(cov_inv)).dot(f)/2 +
+                    2 * np.sum(np.log(np.diag(cov_l)))/2)
+
+        def hessian(f):
+            """Hessian of the log p(y|f)"""
+            f = f.reshape((f.size, 1))
+            diag_vec = (-np.exp(f) / np.square(np.ones(f.shape) + np.exp(f)))
+            return -np.diag(diag_vec.reshape((diag_vec.size, ))) + cov_inv
+
+        def grad(f):
+            f = f.reshape((f.size, 1))
+            return (-((labels + np.ones(labels.shape))/2 - sigmoid(f)) + cov_inv.dot(f)).reshape((f_0.size,))
+
+        f_0 = np.zeros(labels.shape)
+        f_0 = f_0.reshape((f_0.size,))
+        # f_res = op.minimize(fun=loss, x0=f_0, method='Newton-CG',
+        #                     jac=grad, hess=hessian, options={'disp': False, 'maxiter': 100})
+        f_res = op.minimize(loss, f_0, args=(), method='L-BFGS-B', jac=grad,
+                            options={'gtol': 1e-5, 'disp': False, 'maxiter': 100})
+        f_opt = f_res['x']
+        return f_opt, hessian(f_opt) - cov_inv
+
+    @staticmethod
+    def _class_get_ml_partial_derivative(f_opt, k_inv, ancillary_mat, dk_dtheta_mat):
+        """
+        :param f_opt: posterior mode
+        :param k_inv: inverse covariance matrix at data points
+        :param ancillary_mat: (W^(-1) + K)^(-1), where W is hess_opt
+        :param dk_dtheta_mat: the matrix of partial derivatives of the covariance function at data points
+        with respect to hyper-parameter theta
+        :return: marginal likelihood partial derivative with respect to hyper-parameter theta
+        """
+        return ((((f_opt.T.dot(k_inv)).dot(dk_dtheta_mat)).dot(k_inv)).dot(f_opt) -
+                np.trace(ancillary_mat.dot(dk_dtheta_mat)))/2
+
+    def _class_get_ml_grad(self, points, cov_inv, f_opt, anc_mat, params):
+        """
+        :param points: data points array
+        :param cov_inv: inverse covariance matrix
+        :param params: hyper-parameters vector
+        :param f_opt: posterior mode
+        :param anc_mat: ancillary matrix
+        :return: marginal likelihood gradient with respect to hyper-parameters
+        """
+        derivative_matrix_list = self.covariance_obj.get_derivative_function_list(params)
+        noise_derivative = 2 * params[-1] * np.eye(points.shape[1])
+        return np.array([self._class_get_ml_partial_derivative(f_opt, cov_inv, anc_mat,
+                                                               covariance_mat(func, points, points))
+                         for func in derivative_matrix_list] +
+                        [self._class_get_ml_partial_derivative(f_opt, cov_inv, anc_mat,  noise_derivative)])
+
+    def _class_oracle(self, points, f_opt, hess_opt, params):
+        """
+        :param points: data points array
+        :param f_opt: posterior mode
+        :param hess_opt: some weird hessian (-\nabla \nabla \log p(y|f_opt))
+        :param params: hyper-parameters vector
+        """
+        cov_obj = copy.deepcopy(self.covariance_obj)
+        cov_obj.set_params(params)
+        cov_fun = cov_obj.covariance_function
+        points_cov = covariance_mat(cov_fun, points, points)
+        points_l = np.linalg.cholesky(points_cov)
+        points_l_inv = np.linalg.inv(points_l)
+        points_cov_inv = points_l_inv.T.dot(points_l_inv)
+
+        b_mat = self._get_b(points_cov, hess_opt)
+        marginal_likelihood = self._class_get_ml(f_opt, b_mat, points_cov_inv)
+
+        anc_mat = np.linalg.inv(np.linalg.inv(hess_opt) + points_cov)
+        gradient = self._class_get_ml_grad(points, points_cov_inv, f_opt, anc_mat, params)
+
+        return marginal_likelihood, gradient
+
+    def _class_find_hyper_parameters(self, points, labels, max_iter=10):
+        cov_obj = copy.deepcopy(self.covariance_obj)
+        cov_fun = cov_obj.covariance_function
+        bnds = ((1e-2, None), (1e-2, None), (1e-5, None))
+        w0 = self.covariance_obj.get_params()
+
+        def func(w):
+            loss, _ = self._class_oracle(points, f_opt, hess_opt, w)
+            return -loss
+
+        def grad(w):
+            _, gradient = self._class_oracle(points, f_opt, hess_opt, w)
+            return -gradient
+
+        for i in range(max_iter):
+            points_cov = covariance_mat(cov_fun, points, points)
+            points_l = np.linalg.cholesky(points_cov)
+            points_l_inv = np.linalg.inv(points_l)
+            points_cov_inv = points_l_inv.T.dot(points_l_inv)
+            det_k = 2 * np.sum(np.log(np.diag(points_l)))
+
+            f_opt, hess_opt = self._get_laplace_approximation(labels, points_cov_inv, points_l)
+            w_res = op.minimize(func, w0, args=(), method='L-BFGS-B', jac=grad, bounds=bnds,
+                                options={'ftol': 1e-5, 'disp': False, 'maxiter': 1})
+            if (np.linalg.norm(w_res['x'] - w0) < 10e-4):
+                break
+            print("Iteration ", i, ": ", np.linalg.norm(w_res['x'] - w0))
+            w0 = w_res['x']
+            cov_obj.set_params(w0)
+        self.covariance_obj = copy.deepcopy(cov_obj)
+
+    def _class_predict(self, test_points, training_points, training_labels):
+
+        cov_fun = self.covariance_obj.covariance_function
+        points_cov = covariance_mat(cov_fun, training_points, training_points)
+        points_l = np.linalg.cholesky(points_cov)
+        points_l_inv = np.linalg.inv(points_l)
+        points_cov_inv = points_l_inv.T.dot(points_l_inv)
+        f_opt, hess_opt = self._get_laplace_approximation(training_labels, points_cov_inv, points_l)
+        k_test_x = covariance_mat(cov_fun, test_points, training_points)
+
+        f_test = np.dot(np.dot(k_test_x, points_cov_inv), f_opt)
+        loc_y_test = np.sign(f_test.reshape((f_test.size, 1)))
+
+        return loc_y_test
