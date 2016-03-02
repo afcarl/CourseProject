I finally made the svi method work (the way it is described in the article). Now, it does not optimize the kernel wrt noise variance, but I'll fix it soon. I also added the stochastic gradient descent optimization method in the optimization module

Code/gaussian_process_regression.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 from gaussian_process import GP, minimize_wrapper
 from covariance_functions import CovarianceFamily, sigmoid
-from optimization import gradient_descent
+from optimization import gradient_descent, stochastic_gradient_descent
 
 
 class GPR(GP):
@@ -394,7 +394,7 @@ class GPR(GP):
 
 
 
-    def _svi_fit(self, data_points, target_values, num_inputs=0, inputs=None, max_iter=1):
+    def _svi_fit(self, data_points, target_values, num_inputs=0, inputs=None, max_iter=10):
         """
         A method for optimizing hyper-parameters (for fixed inducing points), based on stochastic variational inference
         :param data_points: training set objects
@@ -420,113 +420,40 @@ class GPR(GP):
 
         # Initializing variational (normal) distribution parameters
         mu = np.zeros((m, 1))
-        # mu = np.load("mean.npy")[:num_inputs, :]
-
-        sigma = np.eye(m)
-        # sigma = np.load("cov.npy")[:num_inputs, :num_inputs]
-        sigma_inv = np.linalg.inv(sigma)
-        # sigma_inv = np.eye(m)
+        # sigma = np.eye(m)
+        sigma_inv = np.eye(m)
 
         # Canonical parameters initialization
         eta_1 = sigma_inv.dot(mu)
         eta_2 = - sigma_inv / 2
 
-        # Expectation parameters
-        # beta_1 = mu
-        # beta_2 = mu.dot(mu.T) + sigma
-
-        #ELBO check
-
-        # theta = self.covariance_obj.get_params()[:-1]
-        # param_vec = self._svi_get_parameter_vector(theta, beta_1, beta_2)
-        # param_vec = np.load("res.npy")
-        # theta, beta_1, beta_2 = self._svi_get_parameters(param_vec)
-
-        # # Debug
         sigma_n = self.covariance_obj.get_params()[-1]
-        # cov_fun = self.covariance_obj.covariance_function
-        # K_mm = cov_fun(inputs, inputs)
-        # K_mn = cov_fun(inputs, data_points)
-        # L = np.linalg.cholesky(K_mm)
-        # L_inv = np.linalg.inv(L)
-        # K_mm_inv = L_inv.T.dot(L_inv)
-        # Lambda = K_mm_inv.dot(K_mn.dot(K_mn.T.dot(K_mm_inv))) / sigma_n + K_mm_inv
-        # print(sigma_inv/2 - Lambda/2)
-        # sigma = np.linalg.inv(Lambda)
-        # sigma_inv = Lambda
-        # mu = sigma.dot(K_mm_inv.dot(K_mn.dot(y)))/sigma_n
-        # beta_1 = mu
-        # beta_2 = mu.dot(mu.T) + sigma
-        # theta = self.covariance_obj.get_params()[:-1]
-        # param_vec = self._svi_get_parameter_vector(theta, beta_1, beta_2)
-
-        # mu = np.load("mean.npy")
-        # sigma = np.load("cov.npy")
-
         theta = self.covariance_obj.get_params()[:-1]
         param_vec = self._svi_get_parameter_vector(theta, eta_1, eta_2)
 
-        def fun(x):
-            full_loss = 0
-            full_grad = np.zeros((len(param_vec),))
-            for i in range(n):
-                oracle = self._svi_elbo_approx_oracle(data_points, target_values, inputs, parameter_vec=x, index=i)
-                full_loss += oracle[0]
-                full_grad += oracle[1]
-            # print(full_grad)
-            # exit(0)
-            return -full_loss, -np.array(full_grad)
+        # def fun(x):
+        #     full_loss = 0
+        #     full_grad = np.zeros((len(param_vec),))
+        #     for i in range(n):
+        #         oracle = self._svi_elbo_approx_oracle(data_points, target_values, inputs, parameter_vec=x, index=i)
+        #         full_loss += oracle[0]
+        #         full_grad += oracle[1]
+        #     return -full_loss, -np.array(full_grad)
 
-        # print(fun(param_vec))
-
-        # grad = fun(param_vec)[1]
-        # for i in range(len(param_vec)):
-        #     x_i = np.copy(param_vec)
-        #     x_i[i] += 1e-7
-        #     estimation = (fun(x_i)[0] - fun(param_vec)[0]) * 1e7
-        #     print(estimation, "\t", grad[i], '\t', np.abs(grad[i] - estimation))
-        #
-        # exit(0)
-
-
-        #
-        # print(param_vec[2])
-        # fun_vals = []
-        # param_vals = []
-        # for i in range(7):#range(len(param_vec)):
-        #     diff = np.exp(-i)
-        #     x_i = np.copy(param_vec)
-        #     x_i[2] += diff
-        #     fun_vals.append((fun(x_i)[0])[0, 0])
-        #     param_vals.append(-i)
-        #
-        # print(fun_vals)
-        # exit(0)
+        def stoch_fun(x, i):
+            return -self._svi_elbo_approx_oracle(data_points, target_values, inputs, parameter_vec=x, index=i)[1]
 
         bnds = tuple(list(self.covariance_obj.get_bounds()[:-1]) + [(None, None)] * (len(param_vec) - len(theta)))
-        res = gradient_descent(oracle=fun, point=param_vec, bounds=bnds, options={'maxiter': 100, 'verbose': True,
-                                                                                  'print_freq':10, 'maxstep': 1.0})
+        res = stochastic_gradient_descent(oracle=stoch_fun, n=n, point=param_vec, bounds=bnds,
+                                          options={'maxiter':max_iter, 'batch_size': 10})
         theta, eta_1, eta_2 = self._svi_get_parameters(res)
-        #
-        # res = minimize(fun, param_vec, method='L-BFGS-B', jac=True, bounds=bnds, options={'maxiter': 30, 'iprint':True, 'disp':True})
-        # print(res)
-        # theta, beta_1, beta_2 = self._svi_get_parameters(res['x'])
         theta = list(theta)
         theta.append(sigma_n)
         theta = np.array(theta)
         self.covariance_obj.set_params(theta)
-        # np.save("res.npy", res)
-
         sigma_inv = - 2 * eta_2
         sigma = np.linalg.inv(sigma_inv)
         mu = sigma.dot(eta_1)
-        print(mu)
-
-        # sigma = beta_2 - beta_1.dot(beta_1.T)
-        # sigma_inv = np.linalg.inv(sigma)
-        # mu = beta_1
-        # print(mu)
-        # print(sigma)
         self.inducing_inputs = (inputs, mu, sigma)
 
     def _svi_elbo_approx_oracle(self, data_points, target_values, inducing_inputs, parameter_vec,
@@ -553,23 +480,6 @@ class GPR(GP):
         sigma_L = np.linalg.cholesky(sigma)
         mu = sigma.dot(eta_1)
 
-        # theta, beta_1, beta_2 = self._svi_get_parameters(parameter_vec)
-        # sigma = beta_2 - beta_1.dot(beta_1.T)
-        # print(sigma)
-        # print(np.linalg.eigvals(sigma))
-        # sigma_L = np.linalg.cholesky(sigma)
-        # sigma_inv = np.linalg.inv(sigma)
-        # mu = beta_1
-        # eta_1 = sigma_inv.dot(mu)
-        # eta_2 = - sigma_inv / 2
-
-        # theta, mu, sigma = self._svi_get_parameters(parameter_vec)
-        # # print(np.linalg.eigvals(sigma))
-        # sigma_L = np.linalg.cholesky(sigma)
-        # sigma_inv = np.linalg.inv(sigma)
-        # eta_1 = sigma_inv.dot(mu)
-        # eta_2 = - sigma_inv / 2
-
         old_params = self.covariance_obj.get_params()
         theta = list(theta)
         theta.append(old_params[-1])
@@ -588,7 +498,6 @@ class GPR(GP):
         cov_fun = self.covariance_fun
         params = self.covariance_obj.get_params()
         sigma_n = self.covariance_obj.get_params()[-1]
-        # print(sigma_n)
 
         # Covariance matrices
         K_mm = cov_fun(inducing_inputs, inducing_inputs)
@@ -650,15 +559,10 @@ class GPR(GP):
         dL_dbeta1 = - y_i / sigma_n * (K_mm_inv.dot(k_i)) + eta_1 / N
         dL_dbeta2 = (-Lambda_i - K_mm_inv / N) / 2 - eta_2 / N
 
-        # dL_dsigma = -(Lambda_i + K_mm_inv / N) / 2 + sigma_inv / (2*N)
-        # dL_dmu = -(K_mm_inv.dot(k_i)) * y_i/sigma_n + (Lambda_i + K_mm_inv/N).dot(mu)
-
         grad = grad[:, None]
 
         grad = np.vstack((grad, -dL_dbeta1.reshape(-1)[:, None]))
         grad = np.vstack((grad, dL_dbeta2.reshape(-1)[:, None]))
-        # grad = np.vstack((grad, -dL_dmu.reshape(-1)[:, None]))
-        # grad = np.vstack((grad, dL_dsigma.reshape(-1)[:, None]))
 
         self.covariance_obj.set_params(old_params)
 

Code/optimization.py
@@ -66,71 +66,107 @@ def gradient_descent(oracle, point, bounds=None, options=None):
     :param point: point
     :param bounds: bounds on the variables
     :param options: a dictionary, containing the following fields
-    'maxiter': maximum number of iterations
-    'verbose': a boolean, showing weather or not to print the convergence info
-    'print_freq': the frequency of the convergence messages
-    'g_tol': the tolerance wrt gradient. If the gradient at the current point is
-    smaller than the tollerance, the method stops
-    'step_tol' the tolerance wrt the step length. If the step length at current
-    iteration is less than tollerance, the method stops.
-    'maxstep' is the maximum allowed step length
-    default options : {'maxiter': 1000, 'print_freq':10, 'verbose': False, 'g_tol': 1e-5, 'step_tol': 1e-16,
+        'maxiter': maximum number of iterations
+        'verbose': a boolean, showing weather or not to print the convergence info
+        'print_freq': the frequency of the convergence messages
+        'g_tol': the tolerance wrt gradient. If the gradient at the current point is
+        smaller than the tollerance, the method stops
+        'step_tol' the tolerance wrt the step length. If the step length at current
+        iteration is less than tollerance, the method stops.
+        'maxstep' is the maximum allowed step length
+    default options: {'maxiter': 1000, 'print_freq':10, 'verbose': False, 'g_tol': 1e-5, 'step_tol': 1e-16,
                        'maxstep': 1.0}
     :return: the point with the minimal function value found
     """
-    if 'maxiter' in options.keys():
-        maxiter = options['maxiter']
-    else:
-        maxiter = 1000
+    defaul_options = {'maxiter': 1000, 'print_freq':10, 'verbose': False, 'g_tol': 1e-5, 'step_tol': 1e-16,
+                       'maxstep': 1.0}
+    defaul_options.update(options)
     if 'print_freq' in options.keys():
-        print_freq = options['print_freq']
-    else:
-        print_freq = 10
-    if 'verbose' in options.keys():
-        verbose = options['verbose']
-    elif 'print_freq' in options.keys():
-        verbose = True
-    else:
-        verbose = False
-    if 'g_tol' in options.keys():
-        g_tol = options['g_tol']
-    else:
-        g_tol = 1e-5
-    if 'step_tol' in options.keys():
-        step_tol = options['step_tol']
-    else:
-        step_tol = 1e-16
-    if 'maxstep' in options.keys():
-        maxstep = options['maxstep']
-    else:
-        maxstep = 1.0
+        defaul_options['verbose'] = True
+    options = defaul_options
 
-    step0 = 0.1
-    gamma = 0.55
-
-    step = step0
+    step = 1.0
     x = point
     loss_fun = lambda w: oracle(w)[0]
 
-    for i in range(maxiter):
+    for i in range(options['maxiter']):
         x = project_into_bounds(x, bounds)
-        print('x', x[1])
         loss, grad = oracle(x)
-        if np.linalg.norm(grad) < g_tol:
-            if verbose:
+        if np.linalg.norm(grad) < options['g_tol']:
+            if options['verbose']:
                 print("Gradient norm reached the stopping criterion")
             break
-        # x, step = _linesearch_armiho(fun=loss_fun, gradient=grad, point_loss=loss, bounds=bounds, point=x,
-        #                              step_0=step, maxstep=maxstep)
-        x -= grad * step
-        # x = project_into_bounds(x, bounds)
-        if step < step_tol:
-            if verbose:
+        x, step = _linesearch_armiho(fun=loss_fun, gradient=grad, point_loss=loss, bounds=bounds, point=x,
+                                     step_0=step, maxstep=options['maxstep'])
+        if step < options['step_tol']:
+            if options['verbose']:
                 print("Step length reached the stopping criterion")
             break
-        if not (i % print_freq) and verbose:
+        if not (i % options['print_freq']) and options['verbose']:
             print("Iteration ", i, ":")
             print("\tGradient norm", np.linalg.norm(grad))
             print("\tFunction value", loss)
+    return x
+
+
+def stochastic_gradient_descent(oracle, point, n, bounds=None, options=None):
+    """
+    Stochastic gradient descent optimization method for finite sums
+    :param oracle: an oracle gunction, returning the gradient approximation by one data point,
+    given it's index and the point
+    :param point:
+    :param n: number of training examples
+    :param bounds: bounds on the variables
+    :param options: a dictionary, containing the following fields
+        'maxiter': maximum number of iterations
+        'verbose': a boolean, showing weather or not to print the convergence info
+        'print_freq': the frequency of the convergence messages
+        'batch_size': the size of the mini-batch, used for gradient estimation
+        'step0': initial step of the method
+        'gamma': a parameter of the step length rule. It should be in (0.5, 1). The smaller it
+        is, the more aggressive the method is
+        'update_rate': the rate of shuffling the data points
+    defaul options: {'maxiter': 1000, 'print_freq':10, 'verbose': False, 'batch_size': 1,
+                      'step0': 0.1, 'gamma': 0.55, 'update_rate':1}
+    :return:
+    """
+    defaul_options = {'maxiter': 1000, 'print_freq':10, 'verbose': False, 'batch_size': 1,
+                      'step0': 0.1, 'gamma': 0.55, 'update_rate':1}
+    defaul_options.update(options)
+    if 'print_freq' in options.keys():
+        defaul_options['verbose'] = True
+    options = defaul_options
+
+    batch_size = options['batch_size']
+    step0 = options['step0']
+    gamma = options['gamma']
+
+    batch_num = int(n / batch_size)
+    if n % batch_size:
+        batch_num += 1
+    update_rate = options['update_rate']
+
+    indices = np.random.random_integers(0, n-1, (update_rate * n,))
+    step = step0
+    x = point
+    x = project_into_bounds(x, bounds)
+    grad = 0
+    for epoch in range(options['maxiter']):
+        for i in range(n):
+            index = indices[i]
+            if not (i % batch_size):
+                x -= grad * step
+                x = project_into_bounds(x, bounds)
+                grad = oracle(x, index)
+            else:
+                grad += oracle(x, index)
+
+            # print('x', x[1])
+
+        if not (epoch % update_rate):
+            indices = np.random.random_integers(0, n-1, (update_rate * n,))
+
+        if not (epoch % options['print_freq']) and options['verbose']:
+            print("Epoch ", epoch, ":")
         step = step0 / np.power((i+1), gamma)
     return x