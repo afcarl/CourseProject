Fixed the code related to experiments and performed them once again. Added second derivative for vi method to the text.

Code/Experiments/regression_example.py
@@ -1,29 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# from sklearn import svm, cross_validation
-# from sklearn.cluster import KMeans
-# from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_svmlight_file
+from sklearn.metrics import r2_score
 from GP.gaussian_process_regression import GPR
 from GP.plotting import gp_plot_reg_data
 from GP.covariance_functions import SquaredExponential
+from matplotlib.mlab import griddata
 
-data_params = np.array([1.1, 0.3, 0.1])
+data_params = np.array([1.0, 0.3, 0.1])
 data_covariance_obj = SquaredExponential(data_params)
-# model_params = np.array([10.1, 1.2, 0.1])
-model_params = np.array([1.2, 0.6, 0.1])
-# model_params = np.array([0.6, 0.2, 0.1])
+model_params = np.array([5.1, 0.3, 0.1])
 model_covariance_obj = SquaredExponential(model_params)
 gp = GPR(data_covariance_obj)
 num = 1000
-test_num = 100
-dim = 1
-seed = 21
+test_num = 500
+dim = 5
+seed = 10
+ind_inputs_num = 200
+max_iter = 100
+batch_size = 100
+
 method = 'means'  # possible methods: 'brute', 'vi', 'means', 'svi'
 parametrization = 'cholesky'  # possible parametrizations for svi method: cholesky, natural
 optimizer = 'L-BFGS-B' # possible optimizers: 'SAG', 'FG', 'L-BFGS-B'
-ind_inputs_num = 5
-max_iter = 200
+
+sag_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100}
+fg_options = {'maxiter':max_iter, 'print_freq': 100}
+lbfgsb_options = {'maxiter': max_iter, 'disp': False}
+sg_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100, 'step0': 5e-3, 'gamma': 0.55}
 
 # Generating data points
 np.random.seed(seed)
@@ -32,52 +38,35 @@ if dim == 1:
     x_test = np.linspace(0, 1, test_num)
     x_test = x_test.reshape(1, test_num)
 else:
+    # x_test = np.zeros((dim, test_num))
+    # x_test[:, :] = np.linspace(0, 1, test_num)
+    # for i in range(dim):
+    #     x_test[i, :] = np.linspace(0, 1, test_num / dim)
     x_test = np.random.rand(dim, test_num)
+
 y_tr, y_test = gp.generate_data(x_tr, x_test, seed=seed)
 
 if method == 'brute':
     new_gp = GPR(model_covariance_obj)
     res = new_gp.fit(x_tr, y_tr, max_iter=max_iter)
-
-    # metric = lambda w: new_gp.get_prediction_quality(w, x_tr, y_tr, x_test, y_test)
-    # x_lst, y_lst = res.plot_performance(metric, 'i', freq=1)
-    # plt.plot(x_lst, y_lst, '-ro', label='Full GP')
-    # plt.legend()
-    # plt.show()
-
     predicted_y_test, high, low = new_gp.predict(x_test, x_tr, y_tr)
 
 elif method == 'means' or method == 'vi':
     model_covariance_obj = SquaredExponential(model_params)
     new_gp = GPR(model_covariance_obj, method=method)
     res = new_gp.fit(x_tr, y_tr, num_inputs=ind_inputs_num, max_iter=max_iter)
-
-    # metric = lambda w: new_gp.get_prediction_quality(w, x_tr, y_tr, x_test, y_test)
-    # x_lst, y_lst = res.plot_performance(metric, 't', freq=1)
-    # plt.plot(x_lst, y_lst, '-ro', label='means')
-    # plt.legend()
-    # plt.show()
-
     inducing_points, mean, cov = new_gp.inducing_inputs
     predicted_y_test, high, low = new_gp.predict(x_test)
 
 elif method == 'svi':
     model_covariance_obj = SquaredExponential(model_params)
     new_gp = GPR(model_covariance_obj, method=method, parametrization=parametrization, optimizer=optimizer)
-    res = new_gp.fit(x_tr, y_tr, num_inputs=ind_inputs_num, max_iter=max_iter)
-
-    # metric = lambda w: new_gp.get_prediction_quality(w, x_test, y_test)
-    # x_lst, y_lst = res.plot_performance(metric, 'i', freq=1)
-    # plt.plot(x_lst, y_lst, '-ro', label='svi')
-    # plt.legend()
-    # plt.show()
-
+    res = new_gp.fit(x_tr, y_tr, num_inputs=ind_inputs_num, optimizer_options=lbfgsb_options)
     inducing_points, mean, cov = new_gp.inducing_inputs
     predicted_y_test, high, low = new_gp.predict(x_test)
 
-
 print(new_gp.covariance_obj.get_params())
-print(np.linalg.norm(predicted_y_test - y_test)**2/y_test.size)
+print(r2_score(y_test, predicted_y_test))
 
 if dim == 1:
     gp_plot_reg_data(x_tr, y_tr, 'yo')
@@ -87,5 +76,25 @@ if dim == 1:
     gp_plot_reg_data(x_test, y_test, 'y-')
     if method != 'brute':
         gp_plot_reg_data(inducing_points, mean, 'ro', markersize=12)
-    # gp_plot_reg_data(inducing_points, targets, 'ro', markersize=12)
     plt.show()
+
+elif dim == 2:
+    xi = np.linspace(0, 1, 100)
+    yi = np.linspace(0, 1, 100)
+    zi = griddata(x_tr[0, :], x_tr[1, :], y_tr[:, 0], xi, yi, interp='linear')
+    plt.plot(x_tr[0], x_tr[1], 'yo')
+    if method != 'brute':
+        gp_plot_reg_data(inducing_points[0, :], inducing_points[1, :], 'ro', markersize=12)
+
+    plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
+    plt.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow,
+                      vmax=abs(zi).max(), vmin=-abs(zi).max())
+    plt.colorbar()
+    plt.show()
+# else:
+#     x_test[1:, :] = 0
+#     print(x_test[:, 0])
+#     predicted_y_test, high, low = new_gp.predict(x_test)
+#     gp_plot_reg_data(x_test[0, :][:, None], predicted_y_test, 'bo')
+#     gp_plot_reg_data(x_test[0, :][:, None], y_test, 'yo')
+#     plt.show()

Code/Experiments/svi_variations/big_real.py
@@ -6,15 +6,15 @@ from experiments_svi_variations import run_methods
 from GP.covariance_functions import SquaredExponential
 
 file_name = 'big_real.tikz'
-model_params = np.array([1.0, 0.2, 0.8])
+model_params = np.array([3.0, 2.0, 1.0])
 model_covariance_obj = SquaredExponential(model_params)
-ind_inputs_num = 130
-max_iter = 20
-batch_size = 1000
+ind_inputs_num = 200
+max_iter = 100
+batch_size = 500
 
 x_tr, y_tr = load_svmlight_file('../../../../Programming/DataSets/Regression/cadata(20640, 8).txt')
 data_name = 'cadata'
-title='cadata dataset, n = 16512, d = 8, m=60'
+title='cadata dataset, n = 10000, d = 8, m=200'
 
 x_tr = x_tr.T
 x_tr = x_tr.toarray()
@@ -24,19 +24,19 @@ x_tr = scaler.fit_transform(x_tr)
 
 x_tr = (x_tr + 1) / 2
 y_tr = y_tr.reshape((y_tr.size, 1))
-x_test = x_tr[:, int(x_tr.shape[1] * 0.8):]
-y_test = y_tr[int(x_tr.shape[1] * 0.8):, :]
-y_tr = y_tr[:int(x_tr.shape[1] * 0.8), :]
-x_tr = x_tr[:, : int(x_tr.shape[1] * 0.8)]
+x_test = x_tr[:, 10000:11000]
+y_test = y_tr[10000:11000, :]
+y_tr = y_tr[:10000, :]
+x_tr = x_tr[:, :10000]
 
 
 # Cholesky parametrization
 
-sag_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100}
-fg_options = {'maxiter':max_iter, 'print_freq': 100}
+sag_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 10}
+fg_options = {'maxiter':max_iter, 'print_freq': 10}
 lbfgsb_options = {'maxiter': max_iter, 'disp': False}
-sg_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100, 'step0': 5e-3, 'gamma': 0.55}
+sg_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 10, 'step0': 1e-4, 'gamma': 0.55}
 
 optimizer_options = [sag_options, fg_options, lbfgsb_options, sg_options]
 
-run_methods(x_tr, y_tr, x_test, y_test, model_params, optimizer_options, file_name, ind_inputs_num, title, True)
+run_methods(x_tr, y_tr, x_test, y_test, model_params, optimizer_options, file_name, ind_inputs_num, title, 'r2', True)

Code/Experiments/svi_variations/experiments_svi_variations.py
@@ -5,22 +5,35 @@ from matplotlib2tikz import save
 from GP.covariance_functions import SquaredExponential
 from GP.gaussian_process_regression import GPR
 
+from sklearn.cluster import KMeans
+
 
 def run_methods(train_points, train_targets, test_points, test_targets,
-                model_parameters, optimizer_options, file_name, ind_num, title, show=False):
+                model_parameters, optimizer_options, file_name, ind_num, title, metric_name='r2', show=False):
 
     method = 'svi'
-    # parametrization = 'cholesky'
-    # for optimizer, color, opts in zip(['SAG', 'FG', 'L-BFGS-B'], ['-ro', '-bo', '-go'],
-    #                                   optimizer_options[:-1]):
-    #     print('Optimizer', optimizer)
-    #     model_covariance_obj = SquaredExponential(np.copy(model_parameters))
-    #     new_gp = GPR(model_covariance_obj, method=method, parametrization=parametrization, optimizer=optimizer)
-    #     res = new_gp.fit(train_points, train_targets, num_inputs=ind_num, optimizer_options=opts)
-    #     name = 'svi-' + optimizer
-    #     metric = lambda w: new_gp.get_prediction_quality(w, test_points, test_targets)
-    #     x_lst, y_lst = res.plot_performance(metric, 'i', freq=10)
-    #     plt.plot(x_lst, y_lst, color, label=name)
+    parametrization = 'cholesky'
+
+    means = KMeans(n_clusters=ind_num)
+    means.fit(train_points.T)
+    inputs = means.cluster_centers_.T
+
+    for optimizer, color, opts in zip(['SAG', 'FG', 'L-BFGS-B'], ['-ro', '-bo', '-go'],
+                                      optimizer_options[:-1]):
+        print('Optimizer', optimizer)
+        model_covariance_obj = SquaredExponential(np.copy(model_parameters))
+        new_gp = GPR(model_covariance_obj, method=method, parametrization=parametrization, optimizer=optimizer)
+        res = new_gp.fit(train_points, train_targets, num_inputs=ind_num, optimizer_options=opts, inputs=inputs)
+        name = 'svi-' + optimizer
+        # print(res.params)
+        if metric_name == 'r2':
+            metric = lambda w: new_gp.get_prediction_quality(w, test_points, test_targets)
+        elif metric_name == 'loss':
+            metric = lambda w: new_gp.get_loss(w, train_points, train_targets)
+        else:
+            raise ValueError('Unknown metric', metric)
+        x_lst, y_lst = res.plot_performance(metric, 'i', freq=10)
+        plt.plot(x_lst, y_lst, color, label=name)
 
     parametrization = 'natural'
     print('Natural parametrization')
@@ -29,14 +42,22 @@ def run_methods(train_points, train_targets, test_points, test_targets,
 
     model_covariance_obj = SquaredExponential(np.copy(model_parameters))
     new_gp = GPR(model_covariance_obj, method=method, parametrization=parametrization)
-    res = new_gp.fit(train_points, train_targets, num_inputs=ind_num, optimizer_options=opt_options)
+    res = new_gp.fit(train_points, train_targets, num_inputs=ind_num, optimizer_options=opt_options, inputs=inputs)
     name = 'svi-natural'
-    metric = lambda w: new_gp.get_prediction_quality(w, test_points, test_targets)
+    if metric_name == 'r2':
+        metric = lambda w: new_gp.get_prediction_quality(w, test_points, test_targets)
+    elif metric_name == 'loss':
+        metric = lambda w: new_gp.get_loss(w, train_points, train_targets)
+    else:
+        raise ValueError('Unknown metric')
     x_lst, y_lst = res.plot_performance(metric, 'i', freq=10)
     plt.plot(x_lst, y_lst, '-yo', label=name)
 
     plt.xlabel('Epoch')
-    plt.ylabel('MSE on test data')
+    if metric == 'r2':
+        plt.ylabel('$R^2$-score on test data')
+    elif metric_name == 'loss':
+        plt.ylabel('Loss')
     plt.legend()
     plt.title(title)
     save('../Plots/svi_variations/'+file_name)

Code/Experiments/svi_variations/medium_generated.py
@@ -4,20 +4,27 @@ from experiments_svi_variations import run_methods
 from GP.covariance_functions import SquaredExponential
 from GP.gaussian_process_regression import GPR
 
-file_name = 'medium_generated.tikz'
+
 data_params = np.array([1.1, 0.3, 0.1])
 data_covariance_obj = SquaredExponential(data_params)
-model_params = np.array([20.2, 12.5, 5.8])
+model_params = np.array([5.0, 1.0, 1.0])
 model_covariance_obj = SquaredExponential(model_params)
 gp = GPR(data_covariance_obj)
-num = 2000
+num = 4000
 test_num = 500
-dim = 10
+dim = 5
 seed = 21
-ind_inputs_num = 40
-max_iter = 200
-batch_size = 100
-title='generated dataset, n = 2000, d = 10, m=40'
+ind_inputs_num = 200
+max_iter = 100
+batch_size = 200
+title='generated dataset, n = 4000, d = 5, m=200'
+file_name = 'medium_generated'
+metric = 'loss'
+
+if metric == 'loss':
+    file_name += 'loss'
+file_name += '.tikz'
+
 # Generating data points
 np.random.seed(seed)
 x_tr = np.random.rand(dim, num)
@@ -30,11 +37,11 @@ y_tr, y_test = gp.generate_data(x_tr, x_test, seed=seed)
 
 # Cholesky parametrization
 
-sag_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100}
-fg_options = {'maxiter':max_iter, 'print_freq': 100}
+sag_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 10}
+fg_options = {'maxiter':max_iter, 'print_freq': 10}
 lbfgsb_options = {'maxiter': max_iter, 'disp': False}
-sg_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100, 'step0': 1e-5, 'gamma': 0.9}
+sg_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 10, 'step0': 1e-5, 'gamma': 0.9}
 
 optimizer_options = [sag_options, fg_options, lbfgsb_options, sg_options]
 
-run_methods(x_tr, y_tr, x_test, y_test, model_params, optimizer_options, file_name, ind_inputs_num, title, True)
+run_methods(x_tr, y_tr, x_test, y_test, model_params, optimizer_options, file_name, ind_inputs_num, title, metric, True)

Code/Experiments/svi_variations/small_real.py
@@ -5,12 +5,17 @@ from sklearn.datasets import load_svmlight_file
 from experiments_svi_variations import run_methods
 from GP.covariance_functions import SquaredExponential
 
-file_name = 'small_real.tikz'
-model_params = np.array([1.0, 0.2, 0.8])
+model_params = np.array([1.5, 1.5, 1.0])
 model_covariance_obj = SquaredExponential(model_params)
-ind_inputs_num = 10
+ind_inputs_num = 100
 max_iter = 200
 batch_size = 100
+file_name = 'small_real'
+metric = 'loss'
+
+if metric == 'loss':
+    file_name += 'loss'
+file_name += '.tikz'
 
 x_tr, y_tr = load_svmlight_file('../../../../Programming/DataSets/Regression/bodyfat(252, 14).txt')
 data_name = 'bodyfat'
@@ -34,8 +39,8 @@ x_tr = x_tr[:, : int(x_tr.shape[1] * 0.8)]
 sag_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100}
 fg_options = {'maxiter':max_iter, 'print_freq': 100}
 lbfgsb_options = {'maxiter': max_iter, 'disp': False}
-sg_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100, 'step0': 7e-4, 'gamma': 0.55}
+sg_options = {'maxiter':max_iter, 'batch_size': batch_size, 'print_freq': 100, 'step0': 1e-4, 'gamma': 0.55}
 
 optimizer_options = [sag_options, fg_options, lbfgsb_options, sg_options]
 
-run_methods(x_tr, y_tr, x_test, y_test, model_params, optimizer_options, file_name, ind_inputs_num, title, True)
+run_methods(x_tr, y_tr, x_test, y_test, model_params, optimizer_options, file_name, ind_inputs_num, title, metric, True)

Code/GP/gaussian_process_regression.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.metrics import r2_score
 
 from GP.covariance_functions import CovarianceFamily
 from GP.gaussian_process import GP
@@ -613,6 +614,16 @@ class GPR(GP):
         self.inducing_inputs = (inputs, mu, sigma)
         return GPRRes(deepcopy(w_list), time_lst=deepcopy(time_list))
 
+    def _svi_get_loss(self, params, data_points, target_values):
+        new_gp = deepcopy(self)
+        n = target_values.size
+        def fun(x):
+            fun, grad = new_gp._svi_elbo_batch_approx_oracle(data_points, target_values, new_gp.inducing_inputs[0],
+                                                             parameter_vec=x, indices=list(range(n)))
+            return -fun, -grad
+        return fun(params)[0][0,0]
+
+
     def _svi_elbo_batch_approx_oracle(self, data_points, target_values, inducing_inputs, parameter_vec,
                                        indices):
         """
@@ -765,7 +776,8 @@ class GPR(GP):
         new_gp.covariance_obj.set_params(theta)
         new_gp.inducing_inputs = (new_gp.inducing_inputs[0], mu, Sigma)
         predicted_y_test, _, _ = new_gp.predict(test_points)
-        return np.linalg.norm(predicted_y_test - test_targets)**2/test_targets.size
+        return r2_score(test_targets, predicted_y_test)
+        # return np.linalg.norm(predicted_y_test - test_targets)**2/test_targets.size
 
     def _inducing_points_predict(self, test_points):
         """
@@ -827,7 +839,7 @@ class GPR(GP):
         """
         Returns prediction quality on the test set for the given kernel (and inducing points) parameters
         :param params: parameters
-        :return: prediction MSE
+        :return: prediction r2
         """
         if self.method == 'means':
             return self._means_get_prediction_quality(*args, **kwargs)
@@ -837,3 +849,14 @@ class GPR(GP):
             return self._brute_get_prediction_quality(*args, **kwargs)
         else:
             raise ValueError('Wrong method')
+
+    def get_loss(self, *args, **kwargs):
+        """
+        Returns loss (ELBO) kernel (and inducing points) parameters
+        :return: loss
+        """
+        if self.method == 'svi':
+            return self._svi_get_loss(*args, **kwargs)
+        else:
+            raise ValueError('Wrong method')
+

Code/GP/optimization.py
@@ -90,7 +90,7 @@ def gradient_descent(oracle, point, bounds=None, options=None):
     step = 1.0
     x = point
     loss_fun = lambda w: oracle(w)[0]
-    x_lst = [x]
+    x_lst = [np.copy(x)]
     time_lst = [0]
     start = time.time()
 
@@ -103,7 +103,7 @@ def gradient_descent(oracle, point, bounds=None, options=None):
             break
         x, step = _linesearch_armiho(fun=loss_fun, gradient=grad, point_loss=loss, bounds=bounds, point=x,
                                      step_0=step, maxstep=options['maxstep'])
-        x_lst.append(x)
+        x_lst.append(np.copy(x))
         time_lst.append(time.time() - start)
         if step < options['step_tol']:
             if options['verbose']:
@@ -157,7 +157,7 @@ def stochastic_gradient_descent(oracle, point, n, bounds=None, options=None):
     step = step0
     x = point
     x = project_into_bounds(x, bounds)
-    x_lst = [x]
+    x_lst = [np.copy(x)]
     time_lst = [0]
     start = time.time()
     for epoch in range(options['maxiter']):
@@ -166,7 +166,7 @@ def stochastic_gradient_descent(oracle, point, n, bounds=None, options=None):
             grad = oracle(x, new_indices)
             x -= grad * step
             x = project_into_bounds(x, bounds)
-        x_lst.append(x)
+        x_lst.append(np.copy(x))
         time_lst.append(time.time() - start)
 
         if not (epoch % update_rate):
@@ -234,7 +234,7 @@ def stochastic_average_gradient(oracle, point, n, bounds=None, options=None):
 
     batch_size = options['batch_size']
     l = 1.0
-    eps = 1e-2
+    eps = 0.5
 
     def update_lipschitz_const (l, point, cur_loss=None, cur_grad=None):
         if cur_loss is None or cur_grad is None:
@@ -290,7 +290,7 @@ def stochastic_average_gradient(oracle, point, n, bounds=None, options=None):
     x = point
     x = project_into_bounds(x, bounds)
     batch_oracle = BatchOracle(n=n, batch_size=batch_size)
-    x_lst = [x]
+    x_lst = [np.copy(x)]
     time_lst = [0]
     start = time.time()
 
@@ -301,14 +301,14 @@ def stochastic_average_gradient(oracle, point, n, bounds=None, options=None):
             direction = batch_oracle.update_gradients(grad)
             if l == 0:
                 return x
-            x -= direction / l
+            x -= direction /(16 * l)
             x = project_into_bounds(x, bounds)
-        x_lst.append(x)
+        x_lst.append(np.copy(x))
         time_lst.append(time.time() - start)
         if not (epoch % options['print_freq']) and options['verbose']:
             print("Epoch ", epoch, ":")
             print("\tLipschitz constant estimate:", l)
-            print("\t", x[:2])
+            print("\t", x[:2])    # print(x_lst)
     return x, x_lst, time_lst
 
 